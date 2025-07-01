1a8936bd29ec21e96f85b444dad38fce46e825d2
Rocafuerte330 <jose.roman8253@gmail.com>
2025-06-30 20:33:13 -0500

    Prueba conexión a base de datos

---
+++ Prueba.py
@@ -0,0 +1,15 @@
+#Conexion a base de datos
+import mysql.connector
+
+SERVER = "localhost"
+USER = "BDpython1"
+PASSWD = "bal8bObG)uvxHbHp"
+DB = "BDpython1"
+cnx = mysql.connector.connect(user = USER, password = PASSWD, host = SERVER , database = DB )
+cursor = cnx.cursor()
+
+
+sql = "SELECT * FROM Usuarios"
+cursor.execute(sql)
+results = cursor.fetchall()
+print(results)
